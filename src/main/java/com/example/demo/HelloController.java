857e733522877f816eb37e1cf0477c1e1540fb7c
dylan <405785568@qq.com>
2023-08-31 13:37:02 +0800

    hello world

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+
+public class HelloController {
+
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+}
